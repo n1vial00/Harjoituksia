5046a885fea0212d86ceb75614074133aa119540
Aleksi Viitanen <n1vial00@students.oamk.fi>
2021-12-25 20:57:42 +0200

    Harjoitus 2 lisätty copyright merkintä

--- REST JSON AJAX/js/code.js
+++ REST JSON AJAX/js/code.js
@@ -21,13 +21,14 @@ function get_joke_of_the_day() {
         let joke = JSON.parse(xhttp.response)
 	    if (this.readyState == 4 && this.status == 200) {
 	     // Access the result here
-	     document.querySelector(".joke").textContent = joke.contents.jokes[0].joke.text;
+	     document.querySelector(".joke").innerHTML = joke.contents.jokes[0].joke.text + "<br><br>&copy;" + joke.contents.copyright;
 	 }
     };
-    xhttp.open("GET", "https://api.jokes.one/jod?category=animal", true);
+    xhttp.open("GET", "https://api.jokes.one/jod?category=blonde", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.setRequestHeader("X-JokesOne-Api-Secret", "YOUR API HERE");
     xhttp.send();
 }
 
 get_joke_of_the_day()
+
